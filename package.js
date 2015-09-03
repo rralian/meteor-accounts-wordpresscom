7ab3d1bb2fb5c59182acea2057d21a50a8890065
Bob Ralian <bob.ralian@gmail.com>
2015-09-03 01:24:48 -0500

    initial commit

---
+++ package.js
@@ -0,0 +1,24 @@
+Package.describe({
+  name: 'rralian:accounts-wordpresscom',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'Login service for WordPress.com accounts',
+  version: '0.0.1',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.use('accounts-base', ['client', 'server']);
+  // Export Accounts (etc) to packages using this one.
+  api.imply('accounts-base', ['client', 'server']);
+  api.use('accounts-oauth', ['client', 'server']);
+  api.use('rralian:wordpresscom', ['client', 'server']);
+
+  api.addFiles('wordpresscom_login_button.css', 'client');
+
+  api.addFiles("wordpresscom.js");
+});
